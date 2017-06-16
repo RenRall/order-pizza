2400a40972a39dfd012996201a5d69db0d2a816f
Rena Rallis <renarallis@msn.com>
2017-06-16 11:05:49 -0700

    add a pizza object constructor with properties for toppings and size

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// business logic
+function Pizza (size, meat, cheese, veggie) {
+  this.size = size;
+  this.meat = meat;
+  this.cheese = cheese;
+  this.veggie = veggie;
+}
+
+
+
+
+
+
+
+
+
+
+// user interface logic
+$(document).ready(function() {
+
+
+var meat = {name: "sausage", "pepperoni", "CanadianBacon", "chicken"};
+var cheese = {name: "mozzarella", "provolone", "parmesan";
+var veggie = {name: "olives", "mushrooms", "onions", "peppers", "artichoke"};
+
+var pizza1 = {size: "large", meat: sausage, cheese: mozzarella, veggie: [olives, mushrooms]}
+
+
+
+
+
+
+
+
+}
