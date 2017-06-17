182fc98cd5e3025bd7382c1c056275d71280e450
Rena Rallis <renarallis@msn.com>
2017-06-17 16:58:30 -0700

    add business logic and calcs for pizza orders

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,49 @@
 // business logic
-size1 = 0;
-size2 = 0;
-size3 = 0;
-size4 = 0;
-cheese = 0;
-meat = 0;
-veggie = 0;
-
-
-function Pizza (size1, size2, size3, size4, cheese, meat, veggie) {
-  this.size1 = size1;
-  this.size2 = size2;
-  this.size3 = size3;
-  this.size4 = size4;
+//constructor - pizza
+function Pizza (size, cheese, meat, veggie) {
+  this.size = size;
   this.cheese = cheese;
   this.meat = meat;
   this.veggie = veggie;
 }
 
-Pizza.prototype.cost = function(transactionAmount) {
-  return this.size1 + this.size2 + this.size3 + this.size4 + this.cheese + this.meat + this.veggie;
+Pizza.prototype.cost = function() {
+  //return this.size + this.cheese + this.meat + this.veggie;
+  var sizes = {"small" : 3, "medium" : 4, "large" : 5, "family" : 6};
+  var pieCost = sizes[this["size"]];
+  var cheeseCost = 2 * this["cheese"].length;
+  var meatCost = 3 * this["meat"].length;
+  var veggieCost = 1 * this["veggie"].length;
+  return pieCost + cheeseCost + meatCost + veggieCost;
 }
-
 
 // user interface logic
 $(document).ready(function() {
-  var size1 = "small", price = 3;
-  var size2 = "medium", price = 4;
-  var size3 = "large", price = 5;
-  var size4 = "family", price = 6;
-  var cheese = price 2;
-  var meat = price 3;
-  var veggie = price 1;
 
   $("form#pizzaOrder").submit(function(event) {
-    $("result").empty();
+    $("#orderTotal").empty();
     event.preventDefault();
+    //get user selections to pass to constructor
+    var size = $('input[name=size]:checked').val();
 
+    var cheese = new Array();
+    $('input[name=cheese]:checked').each(function() {
+      cheese.push($(this).val());
+    });
 
-    var userInput = parseInt($("input#user-input").val());
-
-  });
+    var meat = new Array();
+    $('input[name=meat]:checked').each(function() {
+      meat.push($(this).val());
+    });
+    // alert(meat)
+    var veggies = new Array();
+    $('input[name=veggies]:checked').each(function() {
+      veggies.push($(this).val());
+    });
+    //call the pizza constructor
+    var order = new Pizza (size, cheese, meat, veggies);
+    //get price for this order by calling the prototype cost function
+    var orderCost = order.cost();
+    $("#orderTotal").text( "Your total order is: $ " + orderCost);
+  }); //end sub
 });
-
-var pizza = { size: [small, medium, large, family], cheese: [mozzarella, provolone, parmesan, asiago], meat: [sausage, pepperoni, CanadianBacon, chicken], veggie: [olives, mushrooms, onions, peppers, artichokes] }
-
-
-// var size = {size: "small", "medium", "large", "family"}
-// var cheese = {name: "mozzarella", "provolone", "parmesan"};
-// var meat = {name: "sausage", "pepperoni", "CanadianBacon", "chicken"};
-// var veggie = {name: "olives", "mushrooms", "onions", "peppers", "artichoke"};
-
-
-
-
-
-
-
-
-
-}
